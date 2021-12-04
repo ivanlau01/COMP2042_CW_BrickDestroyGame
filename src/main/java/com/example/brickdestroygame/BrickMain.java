9f4269c0144a8b6b8617961056c360d2122db8a6
ivanlau01 <qm5796@gmail.com>
2021-12-04 18:19:56 +0800

    Initial commit with a home page

---
+++ src/main/java/com/example/brickdestroygame/BrickMain.java
@@ -0,0 +1,33 @@
+package com.example.brickdestroygame;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import java.io.IOException;
+import java.util.Objects;
+
+public class BrickMain extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        System.out.println(System.getProperty("user.dir"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("HomePage.fxml")));
+        Scene scene = new Scene(root, 450, 300);
+
+        Image icon = new Image("brick.jpg");
+        stage.getIcons().add(icon);
+        stage.setTitle("Brick Destroy Game");
+        stage.setResizable(false);
+
+        stage.setScene(scene);
+        stage.show();
+
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
